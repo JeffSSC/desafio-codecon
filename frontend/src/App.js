e4161cfc18498734ae4b085685330d67b6a03def
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-04-14 19:55:05 -0300

    front quase pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Enquete</h1>
-      <main className=''>
-      <h2>Opção </h2>
-        {/* <div className='progresso1'>{`${opcao1}`}</div> */}
-        <div className='progresso1'>y%</div>
-        <button className='votar1'>Votar</button>
-      <h2>Opção 2</h2>
-        {/* <div className='progresso2'>{`${opcao2}`}</div> */}
-        <div className='progresso2'>x%</div>
-        <button className='votar2'>Votar</button>
-      </main>
+        <h1 className='tituloH1'>Enquete</h1>
+        <main className='main'>
+        <h2 className='tituloH2'>Opção </h2>
+          {/* <div className='progresso1'>{`${opcao1}`}</div> */}
+          <div className='firstOption'><p className='txt'>y votos</p></div>
+          <button className='voteBtn'>Votar</button>
+        <h2 className='tituloH2'>Opção 2</h2>
+          {/* <div className='progresso2'>{`${opcao2}`}</div> */}
+          <div className='secondOption'><p className='txt'>x votos</p></div>
+          <button className='voteBtn'>Votar</button>
+        </main>
     </>
   );
 }

28f2242cc331e1b2300d6665881540390d7e9416
joaoretzlaffpx <joao.matos@px.center>
2026-04-14 19:54:39 -0300

    refactor: change from socket.io to ws

---
+++ backend/app.mjs
@@ -0,0 +1,23 @@
+import { WebSocketServer } from 'ws';
+
+const server = new WebSocketServer({ 
+  port: 3000
+});
+
+const clients = new Set();
+
+server.on('connection', (socket) => {
+  clients.add(socket);
+
+  socket.on('message', (message) => {
+    clients.forEach((client) => {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  socket.on('close', () => {
+    clients.delete(socket);
+  });
+});

4b2ff9c700ba8977f96d7d14efcfe0a8ffd707a0
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-04-14 20:36:12 -0300

    front quase pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,19 @@ import WbeSocket from "ws";
 
 function App() {
   const ws = new WebSocket("ws:localhost:8000");
-  
-  if (!ws){
+
+  console.log(ws)
+
+  if (!ws) {
     console.log("ERROOOOOOOO 🚫")
   }
 
   async function vote(voteOption) {
+    let optionObject = {
+      option: voteOption
+    }
     ws.on('open', function open() {
-    ws.send({ option: voteOption });
+      ws.send({ option: optionObject });
     });
   }
 
@@ -19,20 +24,20 @@ function App() {
     <body className="body">
       <h1 className="tituloH1">Enquete</h1>
       <main className="main">
-        <h2 className="tituloH2">Opção 1</h2>
+        <h2 className="tituloH2">Opção A</h2>
         {/* <div className='progresso1'>{`${opcao1}`}</div> */}
         <div className="firstOption">
           <p className="txt">y votos</p>
         </div>
-        <button className="voteBtn" onClick={vote(1)}>
+        <button className="voteBtn" onClick={vote("opcao a")}>
           <p className="txt">Votar</p>
         </button>
-        <h2 className="tituloH2">Opção 2</h2>
+        <h2 className="tituloH2">Opção B</h2>
         {/* <div className='progresso2'>{`${opcao2}`}</div> */}
         <div className="secondOption">
           <p className="txt">x votos</p>
         </div>
-        <button className="voteBtn" onClick={vote(2)}>
+        <button className="voteBtn" onClick={vote("opcao b")}>
           <p className="txt">Votar</p>
         </button>
       </main>

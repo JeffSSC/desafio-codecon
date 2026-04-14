9430e9d0ebcf2eb5d8550bbff59232cb222766a4
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-04-14 19:11:14 -0300

    front quase pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Enquete</h1>
+      <main className=''>
+      <h2>Opção </h2>
+        {/* <div className='progresso1'>{`${opcao1}`}</div> */}
+        <div className='progresso1'>y%</div>
+        <button className='votar1'>Votar</button>
+      <h2>Opção 2</h2>
+        {/* <div className='progresso2'>{`${opcao2}`}</div> */}
+        <div className='progresso2'>x%</div>
+        <button className='votar2'>Votar</button>
+      </main>
+    </>
   );
 }
 

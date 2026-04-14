f0081a2855a8a4bf40588150741aac4b7d924914
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-04-14 20:58:10 -0300

    front quase pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,57 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import WbeSocket from "ws";
 
 function App() {
-  const ws = new WebSocket("ws:localhost:8000");
+  // const ws = new WebSocket("ws:localhost:8000");
 
-  console.log(ws)
+  // console.log(ws)
 
-  if (!ws) {
-    console.log("ERROOOOOOOO 🚫")
+  // if (!ws) {
+  //   console.log("ERROOOOOOOO 🚫")
+  // }
+
+  // async function vote(voteOption) {
+  //   let optionObject = {
+  //     option: voteOption
+  //   }
+  //   ws.on('open', function open() {
+  //     ws.send({ option: optionObject });
+  //   });
+  // }
+  let aVotes, bVotes;
+  useEffect(()=>{
+    
+  }, [])
+  function handleVoteA(){
+    let voteA = localStorage.getItem(aVotes)
+    setVoteA(voteA+1)
+    localStorage.setItem(aVotes, voteA)
   }
 
-  async function vote(voteOption) {
-    let optionObject = {
-      option: voteOption
-    }
-    ws.on('open', function open() {
-      ws.send({ option: optionObject });
-    });
+  function handleVoteB(){
+    setVoteB(voteB+1)
   }
+
+  let [voteA, setVoteA] = useState(0);
+  let [voteB, setVoteB] = useState(0);
 
   return (
     <body className="body">
       <h1 className="tituloH1">Enquete 📚</h1>
       <main className="main">
         <h2 className="tituloH2">Opção A 📍</h2>
-        {/* <div className='progresso1'>{`${opcao1}`}</div> */}
         <div className="firstOption">
-          <p className="txt">y votos 🎉</p>
+          <p className="txt">{`${voteA}`} votos 🎉</p>
         </div>
-        <button className="voteBtn" onClick={vote("opcao a")}>
+        <button className="voteBtn" onClick={handleVoteA}>
           <p className="txt">Votar 🖱️</p>
         </button>
         <h2 className="tituloH2">Opção B 📍</h2>
-        {/* <div className='progresso2'>{`${opcao2}`}</div> */}
         <div className="secondOption">
-          <p className="txt">x votos 🎉</p>
+          <p className="txt">{`${voteB}`} votos 🎉</p>
         </div>
-        <button className="voteBtn" onClick={vote("opcao b")}>
+        <button className="voteBtn" onClick={handleVoteB}>
           <p className="txt">Votar 🖱️</p>
         </button>
       </main>

45495e374537a2cd6a25036487b7a9857b9c575b
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-04-14 20:38:14 -0300

    front quase pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,23 +22,23 @@ function App() {
 
   return (
     <body className="body">
-      <h1 className="tituloH1">Enquete</h1>
+      <h1 className="tituloH1">Enquete 📚</h1>
       <main className="main">
-        <h2 className="tituloH2">Opção A</h2>
+        <h2 className="tituloH2">Opção A 📍</h2>
         {/* <div className='progresso1'>{`${opcao1}`}</div> */}
         <div className="firstOption">
-          <p className="txt">y votos</p>
+          <p className="txt">y votos 🎉</p>
         </div>
         <button className="voteBtn" onClick={vote("opcao a")}>
-          <p className="txt">Votar</p>
+          <p className="txt">Votar 🖱️</p>
         </button>
-        <h2 className="tituloH2">Opção B</h2>
+        <h2 className="tituloH2">Opção B 📍</h2>
         {/* <div className='progresso2'>{`${opcao2}`}</div> */}
         <div className="secondOption">
-          <p className="txt">x votos</p>
+          <p className="txt">x votos 🎉</p>
         </div>
         <button className="voteBtn" onClick={vote("opcao b")}>
-          <p className="txt">Votar</p>
+          <p className="txt">Votar 🖱️</p>
         </button>
       </main>
     </body>

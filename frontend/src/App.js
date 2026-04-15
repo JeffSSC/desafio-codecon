dc21bc229f8ba7c2c82bbe38f4af6943850a4064
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-04-14 21:12:37 -0300

    front pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import WbeSocket from "ws";
+import React, { useState } from "react";
 
 function App() {
   // const ws = new WebSocket("ws:localhost:8000");
@@ -19,22 +18,29 @@ function App() {
   //     ws.send({ option: optionObject });
   //   });
   // }
-  let aVotes, bVotes;
-  useEffect(()=>{
-    
-  }, [])
-  function handleVoteA(){
-    let voteA = localStorage.getItem(aVotes)
-    setVoteA(voteA+1)
-    localStorage.setItem(aVotes, voteA)
+
+
+
+  var aVotesLocal = window.localStorage.getItem('aVotes');
+  var bVotesLocal = window.localStorage.getItem('bVotes');
+
+  var aVotes = JSON.parse(aVotesLocal);
+  var bVotes = JSON.parse(bVotesLocal);
+
+  let [voteA, setVoteA] = useState(aVotes ? aVotes : 0);
+  let [voteB, setVoteB] = useState(bVotes ? bVotes : 0);
+
+  function handleVoteA() {
+    setVoteA(voteA + 1)
+    const votes = JSON.stringify(voteA+1)
+    localStorage.setItem("aVotes", votes)
   }
 
-  function handleVoteB(){
-    setVoteB(voteB+1)
+  function handleVoteB() {
+    setVoteB(voteB + 1)
+    const votes = JSON.stringify(voteB+1)
+    localStorage.setItem("bVotes", votes)
   }
-
-  let [voteA, setVoteA] = useState(0);
-  let [voteB, setVoteB] = useState(0);
 
   return (
     <body className="body">
